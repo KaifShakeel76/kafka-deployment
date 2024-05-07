c283c5fd2c5ca67b0ce66c55332999ae6bfdb762
Kaif Shakeel <95091483+KaifShakeel76@users.noreply.github.com>
2024-05-07 12:22:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,7 @@ resource "aws_instance" "kafka_server" {
   ami           = var.ec2_ami
   instance_type = var.ec2_instance_type
   key_name      = var.ec2_key_pair
-  vpc_security_group_ids = [var.default_security_group]  # Specify the default security group ID or replace it with your desired security group
-  subnet_id     = data.aws_subnet.default.id
+  vpc_security_group_ids = [var.default_security_group]
   associate_public_ip_address = true
 
   tags = {
