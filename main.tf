5995b774b0caf0308787e6dd058c1fdf7ab31507
Kaif Shakeel <95091483+KaifShakeel76@users.noreply.github.com>
2024-05-07 12:32:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "kafka_server" {
   ami           = var.ec2_ami
   instance_type = var.ec2_instance_type
   key_name      = var.ec2_key_pair
+  subnet_id     = 
   vpc_security_group_ids = [var.default_security_group]
   associate_public_ip_address = true
 
