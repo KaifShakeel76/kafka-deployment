266b778b0ee3a5b31be4b74e303ec50be0670386
Kaif Shakeel <95091483+KaifShakeel76@users.noreply.github.com>
2024-05-07 12:16:07 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_ids" {
-  value = aws_subnet.subnets[*].id
-}
-
 output "public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.kafka_server.public_ip
 }
